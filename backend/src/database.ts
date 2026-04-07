94687b68a2bcfc6fff220722159a203675e553e6
Paul Lohninger <p.lohninger@students.htl-leonding.ac.at>
2026-04-07 09:31:26 +0200

    updated role in db

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -112,7 +112,7 @@ export class DB {
                                                 Password Text not null,
                                                 PublicName Text,
                                                 Description Text,
-                                                Role Text,
+                                                Role Text not null,
                                                 Title Text,
                                                 Image Text,
                                                 CreatedAt Text not null,
