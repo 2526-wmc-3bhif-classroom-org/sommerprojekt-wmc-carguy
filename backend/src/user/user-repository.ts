473c8997ef7003867d554f6de8bdc76cf9eeaec5
Paul Lohninger <p.lohninger@students.htl-leonding.ac.at>
2026-04-21 09:06:13 +0200

    updated user-repository.ts to implement login

--- backend/src/user/user-repository.ts
+++ backend/src/user/user-repository.ts
@@ -1,27 +1,26 @@
 import { DB } from "../database";
 import { User } from "../../data/model";
 
-
 export class UserRepository {
-    public findAllUsers() {
+    public static findAllUsers(): User[] {
         const db = DB.getInstance();
         const result = db.prepare("SELECT UID as uid, Username as username, Password as password, PublicName as publicName, Description as description, Title as title, Image as image, CreatedAt as createdAt FROM User").all();
         return result;
     }
 
-    public findUserById(id: number) {
+    public static findUserById(id: number) {
         const db = DB.getInstance();
         const result = db.prepare("SELECT UID as uid, Username as username, Password as password, PublicName as publicName, Description as description, Title as title, Image as image, CreatedAt as createdAt FROM User WHERE UID = ?").get(id);
         return result;
     }
 
-    public findUserByUsername(username: string) {
+    public static findUserByUsername(username: string) {
         const db = DB.getInstance();
         const result = db.prepare("SELECT UID as uid, Username as username, Password as password, PublicName as publicName, Description as description, Title as title, Image as image, CreatedAt as createdAt FROM User WHERE Username = ?").get(username);
         return result;
     }
 
-    public create(user: User): void {
+    public static createNewUser(user: User): void {
         const db = DB.getInstance();
 
         db.prepare(`
