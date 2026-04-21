81bd63a97f6d3ab958c786a90888ec0a2b2b363f
Paul Lohninger <p.lohninger@students.htl-leonding.ac.at>
2026-04-21 09:06:30 +0200

    updated user-service.ts to implement login

--- backend/src/user/user-service.ts
+++ backend/src/user/user-service.ts
@@ -1,23 +1,46 @@
-import { User } from "../../data/model";
+import { User, UserInput, UserClaims } from "../../data/model";
 import { UserRepository } from "./user-repository";
+import * as bcrypt from 'bcrypt';
 
 export class UserService {
 
-    constructor(private userRepository: UserRepository) {}
-
-    public getAllUsers(): User[] {
-        return this.userRepository.findAllUsers() as User[];
+    public static getAllUsers(): User[] {
+        return UserRepository.findAllUsers() as User[];
     }
 
-    public getUserById(id: number): User | undefined {
-        return this.userRepository.findUserById(id) as User;
+    public static getUserById(id: number): User | undefined {
+        return UserRepository.findUserById(id) as User;
     }
 
-    public getUserByUsername(userName: string): User | undefined {
-        return this.userRepository.findUserByUsername(userName) as User;
+    public static getUserByUsername(userName: string): User | undefined {
+        return UserRepository.findUserByUsername(userName) as User;
     }
 
-    public createUser(user: User){
-        this.userRepository.create(user);
+    public static createUser(user: User){
+        UserRepository.createNewUser(user);
+    }
+
+    public static checkUserCredentials(user: UserInput): UserClaims | undefined {
+        try {
+            const users: User[] | undefined = UserRepository.findAllUsers();
+            if(users === undefined) throw new Error("No user found");
+            const findUserByEmail: User | undefined = users.find((userSearch) => userSearch.email === user.email);
+            if(findUserByEmail === undefined) throw new Error("No user found");
+            if(!bcrypt.compareSync(user.password, findUserByEmail.password)) throw new Error("Wrong password");
+            return {
+                email: findUserByEmail.email,
+                role: findUserByEmail.role,
+            }
+        }catch(err) {
+            if(err instanceof Error) {
+                throw new Error(err.message);
+            }
+        }
+    }
+
+    public static createNewUser(user: User) {
+        console.log(user);
+        if(user.email === undefined || user.email === null) throw new Error("User credentials are required");
+        UserRepository.createNewUser(user);
     }
 }
